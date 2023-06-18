7252c3c6ad1bd57c77e4d7160956caa458fbfd4d
Kotaro Inoue <k.musaino@gmail.com>
2023-06-18 21:05:29 +0900

    Add precheck
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,7 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-kind create cluster --config config/config.yaml
+if [[ $(kind get clusters) != *"capi-management-cluster"* ]]; then
+    kind create cluster --config config/config.yaml
+fi
 
 # Enable the experimental Cluster topology feature.
 export CLUSTER_TOPOLOGY=true
@@ -14,10 +16,13 @@ clusterctl init --infrastructure docker
 
 kubectl config use-context kind-capi-management-cluster-kind
 
+echo "Waiting required controllers to be ready..."
 kubectl wait pods --all -A -l control-plane=controller-manager --for condition=Ready --timeout 60s
 
 kubectl apply -f clusters/cloudinit
 kubectl apply -f clusters/kubeadm-config-template
 kubectl apply -f clusters/kubelet-extra-args
 
-kubectl wait clusters --all -A --for condition=Ready --timeout 30s
+echo "Waiting for clusters to be ready..."
+kubectl wait clusters --all -A --for condition=Ready --timeout 120s
+
